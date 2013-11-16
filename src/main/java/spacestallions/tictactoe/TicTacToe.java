dac814591fb8f8474573cb72ca96d99e723fb5c8
BjossiAlfreds <bjorna11@ru.is>
2013-11-16 01:56:04 +0000

    Added template TicTacToe classes and updated pom.xml with junit dependency

---
+++ src/main/java/spacestallions/tictactoe/TicTacToe.java
@@ -0,0 +1,10 @@
+
+package spacestallions.tictactoe;
+
+class TicTacToe
+{
+	public static void main (String[] args)
+	{
+		System.out.println ("Template class");
+	}
+}
---
+++ src/test/java/spacestallions/tictactoe/TicTacToeTest.java
@@ -0,0 +1,9 @@
+
+package spacestallions.tictactoe;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TicTacToeTest
+{
+}
