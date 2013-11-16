7b849d76f5f36605b354e18c2bd5baad69b7758e
ghardard <ghardard@gmail.com>
2013-11-16 14:18:21 +0000

    Added simple greet method to TicTacToe.

--- src/main/java/spacestallions/tictactoe/TicTacToe.java
+++ src/main/java/spacestallions/tictactoe/TicTacToe.java
@@ -3,6 +3,9 @@ package spacestallions.tictactoe;
 
 class TicTacToe
 {
+	public String greet(){
+		return "Welcome to TicTacToe!";
+	}
 	public static void main (String[] args)
 	{
 		System.out.println ("Template class");
